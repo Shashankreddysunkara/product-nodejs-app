08f0d50a20f1e09393643a18483ef73829dee1a6
Unknown <unknown@example.com>
2018-08-06 13:41:39 -0500

    stage all files

---
+++ messaging/rabbitmqconnection.js
@@ -0,0 +1,19 @@
+var amqp = require('amqplib/callback_api');
+
+var rabbitmqconn=null;
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+
+  	rabbitmqconn=conn;
+    var q = 'hello';
+    var msg = 'Hello World!';
+
+    ch.assertQueue(q, {durable: false});
+    // Note: on Node 6 Buffer.from(msg) should be used
+    ch.sendToQueue(q, new Buffer(msg));
+    console.log(" [x] Sent %s", msg);
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
+
+module.exports=rabbitmqconn;
